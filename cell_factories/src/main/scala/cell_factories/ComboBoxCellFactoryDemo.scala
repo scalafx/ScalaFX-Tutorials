f96e6388879a839edabcaecc351a9babc278cfec
Jarek Sacha <jpsacha@gmail.com>
2021-01-16 15:21:37 -0500

    Improve demo appearance add more white space

--- cell_factories/src/main/scala/cell_factories/ComboBoxCellFactoryDemo.scala
+++ cell_factories/src/main/scala/cell_factories/ComboBoxCellFactoryDemo.scala
@@ -2,6 +2,7 @@ package cell_factories
 
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
+import scalafx.geometry.Insets
 import scalafx.scene.Scene
 import scalafx.scene.control.ComboBox
 import scalafx.scene.layout.VBox
@@ -17,12 +18,14 @@ object ComboBoxCellFactoryDemo extends JFXApp {
   val items: Seq[Herd] = Seq(Herd("A"), Herd("B"), Herd("C"), Herd("D"), Herd("E"))
 
   stage = new PrimaryStage {
-    scene = new Scene(100, 50) {
+    scene = new Scene(200, 100) {
+      title = "ComboBox CellFactory Demo"
       root = new VBox {
         children = new ComboBox[Herd](items) {
           converter = StringConverter.toStringConverter((h: Herd) => h.name)
           selectionModel.value.select(2)
         }
+        padding = Insets(14, 14, 14, 14)
       }
     }
   }
